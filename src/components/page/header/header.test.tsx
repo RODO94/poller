bbd358657d9d7ce8c158131ee12e5c70ac4fee6c
Rory Doak <rory.doak@gmail.com>
2025-03-26 12:28:07 +0000

    setup header tests

--- src/components/page/header/header.test.tsx
+++ src/components/page/header/header.test.tsx
@@ -1,4 +1,15 @@
-import { test } from "vitest";
+import { describe, it } from "vitest";
 
-test.todo("a user can copy the email", () => {});
-test.todo("a user can copy the github link", () => {});
+describe("the UI", () => {
+  it.todo("shows a header with my name", () => {});
+
+  it.todo("shows relevant links to my work", () => {});
+});
+
+describe("the header interactions", () => {
+  it.todo("a user can copy the email", () => {});
+
+  it.todo("a user can copy the github link", () => {});
+
+  it.todo("a user can copy the repo link");
+});
