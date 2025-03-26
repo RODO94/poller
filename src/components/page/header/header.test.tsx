8642a0b27a4fae87f5ce3e9d50bd6ec25cbb292d
Rory Doak <rory.doak@gmail.com>
2025-03-26 14:42:23 +0000

    setup vitest and add header ui tests

--- src/components/page/header/header.test.tsx
+++ src/components/page/header/header.test.tsx
@@ -1,9 +1,36 @@
-import { describe, it } from "vitest";
+import { beforeAll, describe, expect, it, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Header from "./header";
+import "@testing-library/jest-dom/vitest";
+
+// Mock clipboard API
+const mockClipboard = {
+  writeText: vi.fn(),
+};
+
+Object.defineProperty(navigator, "clipboard", {
+  value: mockClipboard,
+  writable: true,
+});
 
 describe("the UI", () => {
-  it.todo("shows a header with my name", () => {});
+  beforeAll(() => {
+    render(<Header />);
+  });
+  it("shows a header with my name", () => {
+    const nameHeader = screen.getByText("Rory Doak");
+    expect(nameHeader).toBeInTheDocument();
+  });
 
-  it.todo("shows relevant links to my work", () => {});
+  it("shows relevant links to my work", () => {
+    const emailHeader = screen.getByText("rory.doak@gmail.com");
+    const repoHeader = screen.getByText("Project repo");
+    const profileHeader = screen.getByText("Github profile");
+
+    expect(emailHeader).toBeInTheDocument();
+    expect(repoHeader).toBeInTheDocument();
+    expect(profileHeader).toBeInTheDocument();
+  });
 });
 
 describe("the header interactions", () => {
