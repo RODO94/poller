715de67139e052a842865740d4e12c8c7eb69ac5
Rory Doak <rory.doak@gmail.com>
2025-03-26 12:26:01 +0000

    setup shad/cn and vitest

---
+++ src/components/page/header/header.test.tsx
@@ -0,0 +1,4 @@
+import { test } from "vitest";
+
+test.todo("a user can copy the email", () => {});
+test.todo("a user can copy the github link", () => {});
---
+++ src/components/page/header/header.tsx
@@ -0,0 +1,13 @@
+export default function header() {
+  return (
+    <>
+      {/*
+        Need a box containing two sections, 
+        each sections needs to have two lines of text 
+    */}
+      {/* 
+        Additionally we need a central coloured box 
+      */}
+    </>
+  );
+}
