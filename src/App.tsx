2fa1920f6e9ec191b7bbf72f7ab866db2fd945c5
Rory Doak <rory.doak@gmail.com>
2025-03-31 15:04:51 +0100

    switch to proper casing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Header from "./components/page/Headers/Headers";
+import Header from "./components/page/Header/Header";
 import Hero from "./components/page/Hero/Hero";
 import { Toaster } from "./components/ui/sonner";
 import Chat from "./components/page/Chat/Chat";
