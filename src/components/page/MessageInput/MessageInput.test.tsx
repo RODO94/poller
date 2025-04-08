64dff39c20ec4a65936bed063d48a172e3cb7b11
Rory Doak <rory.doak@gmail.com>
2025-04-08 10:59:56 +0100

    add test coverage for sending a message

--- src/components/page/MessageInput/MessageInput.test.tsx
+++ src/components/page/MessageInput/MessageInput.test.tsx
@@ -1,6 +1,8 @@
 import { cleanup, render, screen } from "@testing-library/react";
 import MessageInput from "./MessageInput";
 import userEvent from "@testing-library/user-event";
+import { useStore } from "../../../store/store";
+
 describe("the UI", () => {
   beforeEach(() => {
     render(<MessageInput />);
@@ -29,5 +31,41 @@ describe("the UI", () => {
     await user.type(messageBox, "initial message");
 
     expect(screen.getByRole("button")).not.toBeDisabled();
+  });
+});
+
+describe("the functionality", () => {
+  beforeEach(() => {
+    render(<MessageInput />);
+  });
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("should clear the message box when the send button is clicked", async () => {
+    const user = userEvent.setup();
+    const messageBox = screen.getByLabelText("Add a comment");
+    const sendMessageButton = screen.getByRole("button");
+
+    await user.type(messageBox, "initial message");
+
+    expect(messageBox).toHaveValue("initial message");
+
+    await user.click(sendMessageButton);
+    expect(messageBox).toHaveValue("");
+  });
+
+  it("should call the addMessage function when the send button is clicked", async () => {
+    const addMessageSpy = vi.spyOn(useStore.getState(), "addMessage");
+    const user = userEvent.setup();
+    const messageBox = screen.getByLabelText("Add a comment");
+    const sendMessageButton = screen.getByRole("button");
+
+    await user.type(messageBox, "initial message");
+
+    expect(messageBox).toHaveValue("initial message");
+
+    await user.click(sendMessageButton);
+    expect(addMessageSpy).toHaveBeenCalledTimes(1);
   });
 });
