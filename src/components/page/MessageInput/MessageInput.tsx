ce2980f7b0944152cf332dfd1e578345469d26c7
Rory Doak <rory.doak@gmail.com>
2025-04-08 10:43:41 +0100

    add formik and memoised send button

--- src/components/page/MessageInput/MessageInput.tsx
+++ src/components/page/MessageInput/MessageInput.tsx
@@ -1,44 +1,54 @@
 import { Textarea } from "@/components/ui/textarea";
-import { Button } from "@/components/ui/button";
-import { Send } from "lucide-react";
 import { useStore } from "@/store/store";
-import { useState } from "react";
+import { useFormik } from "formik";
+import { SendButton } from "@/components/ui/SendButton";
 
 export default function MessageInput() {
-  const [message, setMessage] = useState<string>();
-  const addMessage = useStore().addMessage;
-  const handleSend = () => addMessage(String(Math.random().toPrecision(2)));
+  const { addMessage, currentUser } = useStore();
+  const { handleSubmit, setFieldValue, values, resetForm } = useFormik({
+    initialValues: { message: "" },
+    onSubmit: (values) => {
+      if (!values.message) return;
+      const messageId = String(Math.random().toPrecision(2));
+      addMessage(messageId, {
+        [messageId]: {
+          message: {
+            text: values.message,
+            date: new Date(Date.now()).toLocaleString(),
+            reactions: {
+              allReactions: { fire: 0, smile: 0, heart: 0 },
+              userReactions: { fire: false, smile: false, heart: false },
+            },
+          },
+          profile: currentUser,
+        },
+      });
+      resetForm();
+    },
+  });
 
-  console.log(Boolean(message));
+  const isMessageInputEmpty = Boolean(!values.message);
+
   return (
     <section className="flex flex-row justify-between w-full px-16 py-4 items-center gap-2.5">
       <label htmlFor="message-input" hidden>
         Add a comment
       </label>
       <Textarea
-        style={{ width: "100%", fontSize: "10px" }}
-        rows={1}
+        style={{ width: "100%", fontSize: "10px", resize: "none" }}
         name="message-input"
         id="message-input"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
+        cols={5}
+        value={values.message}
+        onChange={(e) => setFieldValue("message", e.target.value)}
         className={
-          "text-black py-1.5 px-1.5 min-w-24 relative bg-white rounded-3xl border-[0.50px] border-white"
+          "text-black py-1.5 px-2.5 min-w-24 relative bg-white rounded-3xl border-[0.50px] border-white"
         }
       />
-      <Button
-        style={{ borderRadius: "50%" }}
-        id="send"
-        onClick={handleSend}
-        variant={!message ? "ghost" : "default"}
-        disabled={Boolean(!message)}
-      >
-        <Send
-          size={12}
-          strokeWidth={1.25}
-          color={!message ? "grey" : "white"}
-        />
-      </Button>
+      <SendButton
+        handleSubmit={handleSubmit}
+        isMessageInputEmpty={isMessageInputEmpty}
+      />{" "}
     </section>
   );
 }
---
+++ src/components/ui/SendButton.tsx
@@ -0,0 +1,29 @@
+import { memo } from "react";
+import { Button } from "./button";
+import { Send } from "lucide-react";
+
+export const SendButton = memo(function SendButton({
+  handleSubmit,
+  isMessageInputEmpty,
+}: {
+  handleSubmit: (e?: React.FormEvent<HTMLFormElement>) => void;
+  isMessageInputEmpty: boolean;
+}) {
+  return (
+    <Button
+      style={{ borderRadius: "50%" }}
+      id="send"
+      onClick={() => {
+        handleSubmit();
+      }}
+      variant={isMessageInputEmpty ? "ghost" : "default"}
+      disabled={Boolean(isMessageInputEmpty)}
+    >
+      <Send
+        size={12}
+        strokeWidth={1.25}
+        color={isMessageInputEmpty ? "grey" : "white"}
+      />
+    </Button>
+  );
+});
